1c613fbd02e674b4d244f3135dfcb00a44b07ed1
Shiro-Kage-12 <104338458+Shiro-Kage-12@users.noreply.github.com>
2022-07-11 03:37:20 +0530

    Update question2.py

--- question2.py
+++ question2.py
@@ -1,8 +1,10 @@
 #program to check if a string is a palindrome or not
-str = input("Enter a word to check whether it is palindrome or not : ")
-str = str.lower()
-str_rev = str[::-1]
-if(str==str_rev):
-    print("The word is palindrome.")
-else:
-    print("The word is not palindrome.")
+def palindrome():
+    str = input("Enter a word to check whether it is palindrome or not : ")
+    str = str.lower()
+    str_rev = str[::-1]
+    if(str==str_rev):
+        print("The word is palindrome.")
+    else:
+        print("The word is not palindrome.")
+palindrome()
