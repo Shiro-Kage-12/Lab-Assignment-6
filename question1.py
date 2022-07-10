af4b21c2f99ed1348ee4c452a52ee0d8492710a0
Shiro-Kage-12 <104338458+Shiro-Kage-12@users.noreply.github.com>
2022-07-11 03:36:02 +0530

    Update question1.py

--- question1.py
+++ question1.py
@@ -1,10 +1,12 @@
 # program to find the perfect number
-n=int(input("Enter a number to find if it is a perfect number or not : "))
-check=0
-for i in range(1,int(n/2)+1):
-    if(n%i==0):
-        check=check+i
-if(n==check):
-    print("The number is a perfect number.")
-else:
-    print("The number is not a perfect number.")
+def perfect_num():
+    n=int(input("Enter a number to find if it is a perfect number or not : "))
+    check=0
+    for i in range(1,int(n/2)+1):
+        if(n%i==0):
+            check=check+i
+    if(n==check):
+        print("The number is a perfect number.")
+    else:
+        print("The number is not a perfect number.")
+perfect_num()
